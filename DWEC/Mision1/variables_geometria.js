bb31eb7b661a2c1009351b7bf78429e8411d035c
Antonio Monzó <87438455+AntonioMonzo4@users.noreply.github.com>
2024-10-04 16:55:00 +0200

    Add files via upload

---
+++ DWEC/Mision1/variables_geometria.js
@@ -0,0 +1,16 @@
+var base=2;
+var altura=4;
+var lado1=6;
+var lado2=8;
+var lado3=10;
+var radio=5;
+
+areaTriangulo(base,altura);
+perimetroTrianguloEquilatero(lado1);
+perimetroTriangulaIsosceles(lado1,lado2);
+perimetroTrianguloEscaleno(lado1,lado2,lado3);
+areaCuadrado(lado1);
+perimetroCuadrado(lado1);
+areaRectangulo(lado1,lado2);
+areaCirculo(radio);
+longitudCircunferencia(radio);
